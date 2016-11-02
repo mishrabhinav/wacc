a11a21f9c30526969aa39bb48911f1de1c922ab4
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-11-02 23:28:53 +0000

    wacc.go: Update grammer string handling
    Signed-off-by: Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>

--- wacc.go
+++ wacc.go
@@ -3,10 +3,19 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"os"
+	"fmt"
 )
 
 func main() {
-	buffer, err := ioutil.ReadFile("try.wacc")
+	if len(os.Args) < 2 {
+		fmt.Printf("%v FILE\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	file, err := os.Open(os.Args[1])
+
+	buffer, err := ioutil.ReadAll(file)
 	if err != nil {
 		log.Fatal(err)
 	}
