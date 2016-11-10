1ff42f198803d99ea1140b3bc98624775603bf87
Alberto Spina <as12015@ic.ac.uk>
2016-11-10 19:12:40 +0000

    flags.go: Fix linewrapping 80 characters

--- flags.go
+++ flags.go
@@ -15,10 +15,14 @@ type Flags struct {
 func (f *Flags) Parse() {
 	flag.StringVar(&f.filename, "file", "", "Input File")
 
-	flag.BoolVar(&f.printPEGTree, "peg", false, "Print PEG tree for the supplied file")
-	flag.BoolVar(&f.printPretty, "pretty", false, "Pretty print the supplied file")
-	flag.BoolVar(&f.printAST, "ast", false, "Print AST for the supplied file")
-	flag.BoolVar(&f.verbose, "verbose", false, "Print different stages during compilation")
+	flag.BoolVar(&f.printPEGTree, "peg", false,
+		"Print PEG tree for the supplied file")
+	flag.BoolVar(&f.printPretty, "pretty", false,
+		"Pretty print the supplied file")
+	flag.BoolVar(&f.printAST, "ast", false,
+		"Print AST for the supplied file")
+	flag.BoolVar(&f.verbose, "verbose", false,
+		"Print different stages during compilation")
 
 	flag.Parse()
 }
