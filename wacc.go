968c659078996d124922fee8892f4ca259dff1b5
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-11-22 19:45:18 +0000

    wacc.go: Add comments for different stages
    Signed-off-by: Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>

--- wacc.go
+++ wacc.go
@@ -16,10 +16,14 @@ import (
 	"os"
 )
 
+// main is the starting point of the compiler
 func main() {
+
+	// Parse all the supplied arguments
 	var flags Flags
 	flags.Parse()
 
+	// Open the input wacc file and read the code
 	file, err := os.Open(flags.filename)
 
 	buffer, err := ioutil.ReadAll(file)
@@ -27,31 +31,37 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Prints compiler stage, if verbose flag is supplied
 	flags.Start()
 
+	// Initialise the Lexer and Parser
 	wacc := &WACC{Buffer: string(buffer), File: flags.filename}
 	wacc.Init()
 
+	// Parse the supplied code and check for errors
 	err = wacc.Parse()
 	if err != nil {
 		log.Print(err)
 		os.Exit(100)
 	}
 
+	// Prints the PEG Tree structure, if peg flag is supplied
 	if flags.printPEGTree {
 		fmt.Println("-- Printing PEG Tree")
 		wacc.PrintSyntaxTree()
 	}
 
+	// Parse the library generated tree and return the sanitized AST
 	ast, err := ParseAST(wacc)
-
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(100)
 	}
 
+	// Prints the AST in pretty format, if appropriate flag supplied
 	flags.PrintPrettyAST(ast)
 
+	// Look for further syntax errors, which were missed by the grammar
 	if retErrs := ast.CheckFunctionCodePaths(); len(retErrs) > 0 {
 		for _, err := range retErrs {
 			fmt.Println(err.Error())
@@ -59,6 +69,7 @@ func main() {
 		os.Exit(100)
 	}
 
+	// Check the semantics of the syntactically correct program
 	if typeErrs := ast.TypeCheck(); len(typeErrs) > 0 {
 		for _, err := range typeErrs {
 			fmt.Println(err.Error())
@@ -66,9 +77,10 @@ func main() {
 		os.Exit(200)
 	}
 
-	//Start Code Generation
+	// Initialise Code Generation
 	armFile := bufio.NewWriter(os.Stdout)
 
+	// Put the assembly code in a file, if assembly flag missing
 	if !flags.printAssembly {
 		armFileHandle, err := os.Create(flags.assemblyfile)
 		if err != nil {
@@ -76,15 +88,20 @@ func main() {
 		}
 
 		defer armFileHandle.Close()
+
 		armFile = bufio.NewWriter(armFileHandle)
 	}
 
+	// Take all the instructions in the channel and push them to the defined
+	// IO Writer
 	for instr := range ast.CodeGen() {
 		fInstr := fmt.Sprintf("%v\n", instr)
 		fmt.Fprint(armFile, fInstr)
 	}
 
+	// Flush the buffered writer
 	armFile.Flush()
 
+	// Prints compiler stage, if verbose flag is supplied
 	flags.Finish()
 }
