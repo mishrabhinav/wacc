501a939b49679b2b91bfe6a0c3340eccced99fae
Szilveszter Székely <szilveszter.szekely15@imperial.ac.uk>
2016-12-07 19:59:42 +0000

    wacc-lang: Introduce framework for optimisation

---
+++ optimisation.go
@@ -0,0 +1,378 @@
+package main
+
+// WACC Group 34
+//
+// optimisation.go: Contains functions to optimise a given AST
+//
+// The File contains functions to optimise a given AST.
+
+// OptimisationContext holds information that can be useful during optimisation
+type OptimisationContext struct {
+}
+
+//------------------------------------------------------------------------------
+// Optimise
+//------------------------------------------------------------------------------
+
+// Optimise for skip statements
+func (m *SkipStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise for block statements
+func (m *BlockStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for DeclareAssignStatement
+func (m *DeclareAssignStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for AssignStatement
+func (m *AssignStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for ReadStatement
+func (m *ReadStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for FreeStatement
+func (m *FreeStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for ReturnStatement
+func (m *ReturnStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for ExitStatement
+func (m *ExitStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for PrintLnStatement
+func (m *PrintLnStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for PrintStatement
+func (m *PrintStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for FunctionCallStat
+func (m *FunctionCallStat) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for IfStatement
+func (m *IfStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for WhileStatement
+func (m *WhileStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for SwitchStatement
+func (m *SwitchStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for DoWhileStatement
+func (m *DoWhileStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for ForStatement
+func (m *ForStatement) Optimise(context *OptimisationContext) Statement {
+	if m.next != nil {
+		m.SetNext(m.next.Optimise(context))
+	}
+
+	return m
+}
+
+//Optimise optimises for PairElemLHS
+func (m *PairElemLHS) Optimise(context *OptimisationContext) LHS {
+	return m
+}
+
+//Optimise optimises for ArrayLHS
+func (m *ArrayLHS) Optimise(context *OptimisationContext) LHS {
+	return m
+}
+
+//Optimise optimises for VarLHS
+func (m *VarLHS) Optimise(context *OptimisationContext) LHS {
+	return m
+}
+
+//Optimise optimises for PairLiterRHS
+func (m *PairLiterRHS) Optimise(context *OptimisationContext) RHS {
+	return m
+}
+
+//Optimise optimises for ArrayLiterRHS
+func (m *ArrayLiterRHS) Optimise(context *OptimisationContext) RHS {
+	return m
+}
+
+//Optimise optimises for PairElemRHS
+func (m *PairElemRHS) Optimise(context *OptimisationContext) RHS {
+	return m
+}
+
+//Optimise optimises for FunctionCallRHS
+func (m *FunctionCallRHS) Optimise(context *OptimisationContext) RHS {
+	return m
+}
+
+//Optimise optimises for ExpressionRHS
+func (m *ExpressionRHS) Optimise(context *OptimisationContext) RHS {
+	return m
+}
+
+//Optimise optimises for NewInstanceRHS
+func (m *NewInstanceRHS) Optimise(context *OptimisationContext) RHS {
+	return m
+}
+
+//------------------------------------------------------------------------------
+// LITERALS AND ELEMENTS OPTIMISATION
+//------------------------------------------------------------------------------
+
+//Optimise optimises for Ident
+func (m *Ident) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for IntLiteral
+func (m *IntLiteral) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BoolLiteralTrue
+func (m *BoolLiteralTrue) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BoolLiteralFalse
+func (m *BoolLiteralFalse) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for CharLiteral
+func (m *CharLiteral) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for StringLiteral
+func (m *StringLiteral) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for NullPair
+func (m *NullPair) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for ArrayElem
+func (m *ArrayElem) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//------------------------------------------------------------------------------
+// UNARY OPERATOR OPTIMISATION
+//------------------------------------------------------------------------------
+
+//Optimise optimises for UnaryOperatorNot
+func (m *UnaryOperatorNot) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for UnaryOperatorNegate
+func (m *UnaryOperatorNegate) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for UnaryOperatorLen
+func (m *UnaryOperatorLen) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for UnaryOperatorOrd
+func (m *UnaryOperatorOrd) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for UnaryOperatorChr
+func (m *UnaryOperatorChr) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//------------------------------------------------------------------------------
+// BINARY OPERATOR OPTIMISATION
+//------------------------------------------------------------------------------
+
+//Optimise optimises for BinaryOperatorMult
+func (m *BinaryOperatorMult) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BinaryOperatorDiv
+func (m *BinaryOperatorDiv) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BinaryOperatorMod
+func (m *BinaryOperatorMod) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BinaryOperatorAdd
+func (m *BinaryOperatorAdd) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BinaryOperatorSub
+func (m *BinaryOperatorSub) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BinaryOperatorGreaterThan
+func (m *BinaryOperatorGreaterThan) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BinaryOperatorGreaterEqual
+func (m *BinaryOperatorGreaterEqual) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BinaryOperatorLessThan
+func (m *BinaryOperatorLessThan) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BinaryOperatorLessEqual
+func (m *BinaryOperatorLessEqual) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BinaryOperatorEqual
+func (m *BinaryOperatorEqual) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BinaryOperatorNotEqual
+func (m *BinaryOperatorNotEqual) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BinaryOperatorAnd
+func (m *BinaryOperatorAnd) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BinaryOperatorBitAnd
+func (m *BinaryOperatorBitAnd) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BinaryOperatorOr
+func (m *BinaryOperatorOr) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for BinaryOperatorBitOr
+func (m *BinaryOperatorBitOr) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for VoidExpr
+func (m *VoidExpr) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//Optimise optimises for ExprParen
+func (m *ExprParen) Optimise(context *OptimisationContext) Expression {
+	return m
+}
+
+//------------------------------------------------------------------------------
+// GENERAL OPTIMISATION UTILITY
+//------------------------------------------------------------------------------
+
+// Optimise generates instructions for functions
+func (m *FunctionDef) Optimise() {
+}
+
+// Optimise generates instructions for the whole program
+func (m *AST) Optimise() {
+}
